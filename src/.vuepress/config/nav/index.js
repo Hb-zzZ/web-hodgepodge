423cc8a348814de4a8b3f97bacc25f4c27097f95
Hb-zzZ <Hb-email@foxmail.com>
2020-11-24 18:21:46 +0800

    ✨ Feat: 新增Mark板块

--- src/.vuepress/config/nav/index.js
+++ src/.vuepress/config/nav/index.js
@@ -1,4 +1,5 @@
 module.exports = [
   { text: '主页', link: '/', icon: 'reco-home' },
+  { text: 'Mark', link: '/views/Mark', icon: 'reco-home' },
   { text: 'Github', link: 'https://github.com/Hb-zzZ/web-hodgepodge', icon: 'reco-github' }
 ]
---
+++ src/views/Mark.md
@@ -0,0 +1,5 @@
+收集一些令人好好品味的链接
+
+## CSS
+
+### [CSS BEM 命名](https://www.zhihu.com/question/21935157) 👉 来自`大猫`的回答
