c48b90e96ed8049b800fc5ce39e3579c8952a266
Hb-zzZ <Hb-email@foxmail.com>
2021-01-06 16:01:59 +0800

    ⬆️  Upgrade: upgrade vuepress

--- src/.vuepress/config/nav/index.js
+++ src/.vuepress/config/nav/index.js
@@ -1,5 +1,5 @@
 module.exports = [
   { text: '主页', link: '/', icon: 'reco-home' },
-  { text: 'Mark', link: '/views/Mark', icon: 'reco-home' },
+  { text: 'Mark', link: '/views/Mark', icon: 'reco-other' },
   { text: 'Github', link: 'https://github.com/Hb-zzZ/web-hodgepodge', icon: 'reco-github' }
 ]
